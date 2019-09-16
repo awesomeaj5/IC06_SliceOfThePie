5c70c25c054cea9770f499ef03426e9bd62e377f
w7264549 <awesomeaj5@gmail.com>
2019-09-16 13:31:37 -0700

    Finished code st of code. Only missing names of lowest and highest

--- src/edu/miracosta/cs112/aort/Main.java
+++ src/edu/miracosta/cs112/aort/Main.java
@@ -1,5 +1,5 @@
 package edu.miracosta.cs112.aort;
-
+import java.text.NumberFormat;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -7,8 +7,9 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-double amount=0 , total = 0, average = 0,  highest = Double.MIN_VALUE, lowest = Double.MAX_VALUE;
-String lowestName, highestName, name;
+double price= 0, amount=0 , total = 0, average = 0,  highest = Double.MIN_VALUE, lowest = Double.MAX_VALUE,
+        highestO = Double.MIN_VALUE, lowestO= Double.MAX_VALUE, averageO = 0, priceO = 0, totalO = 0;
+String lowestName = "", highestName="", lowestNameO = "", highestNameO= "";
         try{
             Scanner file = new Scanner(new File ("USPizzaDataset.csv"));
             String [] parts;
@@ -20,22 +21,51 @@ String lowestName, highestName, name;
               String line = file.nextLine();
               parts = line.split(",");
                 amount++;
-                for (int i = 0; i< parts.length; i++)
-                {
+//                for (int i = 0; i< parts.length; i++)
+//                {
 
-                   total = Double.parseDouble(parts[6]);
-                 if(total > highest)
-                 highest = total;
-                 if (total < lowest)
-                     lowest = total;
-                 name = parts[9];
-                 lowestName = ;
-                }
+                   price = Double.parseDouble(parts[6]);
+                   total+= price;
+
+                 if(price > highest) {
+                     highest = price;
+                     highestName = parts[9];
+                 }
+                 if (price < lowest) {
+                     lowest = price;
+                     lowestName = parts[9];
+                 }
+                 average = total / amount;
+                 if(parts[2].equals("Oceanside"))
+                 {
+
+                     if(price > highestO) {
+                         highestO = price;
+                         highestNameO = parts[9];
+                     }
+                     if (price < lowestO) {
+                         lowestO = price;
+                         lowestNameO = parts[9];
+                     }
+                 }
+
+
+
+
+                //}
+
 
             }
-            System.out.println(amount);
-            System.out.println(lowest);
-            System.out.println(highest);
+            NumberFormat currency = NumberFormat.getCurrencyInstance();
+            System.out.println("Highest priced pizza place nationally is " + highestName);
+            System.out.println("With a price of " + currency.format(highest));
+            System.out.println("The lowest priced pizza place nationally is " + lowestName);
+            System.out.println("With a price of " + currency.format(lowest));
+            System.out.println("The average price of a slice of pizza is " + currency.format(average));
+            System.out.println("The highest priced pizza place in Oceanside is " + highestNameO);
+            System.out.println("At a price of " + currency.format(highestO));
+            System.out.println("The lowest priced pizza place in Oceanside is " + lowestNameO);
+            System.out.println("At a price of " + currency.format(lowestO));
             file.close();
 
         }catch (FileNotFoundException e)
